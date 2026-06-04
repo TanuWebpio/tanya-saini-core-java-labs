26bfca6d29752f1df62cb320907ac5a8166398ac
tanya <tanyaSaini428@gmail.com>
2026-06-04 21:57:31 +0530

    Differences between sum of square & square of sum

---
+++ src/com/tanya/lab1/exp6/Difference.java
@@ -0,0 +1,28 @@
+package com.tanya.lab1.exp6;
+
+import java.util.Scanner;
+
+public class Difference {
+ public static void main(String[] args) {
+	 System.out.println("Enter the value of n: ");
+	 Scanner sc = new Scanner(System.in);
+	 int n = sc.nextInt();
+	 int diff = sumOfSquares(n) - squaresOfSum(n);
+	 System.out.println(diff);
+	
+}
+ public static int sumOfSquares(int n) {
+	 int sum = 0;
+	 for(int i = 1; i<=n; i++) {
+		 sum += i*i;
+	 }
+	 return sum;
+ }
+ public static int squaresOfSum(int n) {
+	 int sum = 0;
+	 for(int i = 0; i<n; i++) {
+		 sum += i;
+	 }
+	 return sum * sum;
+ }
+}
