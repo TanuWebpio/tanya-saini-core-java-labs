4eab1c94d6f438ad0f66c962ec619c5ff2830af1
tanya <tanyaSaini428@gmail.com>
2026-06-04 19:57:17 +0530

    update in ex1

--- src/com/tanya/lab1/ex1/Cube.java
+++ src/com/tanya/lab1/ex1/Cube.java
@@ -6,9 +6,15 @@ public class Cube {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the value of n : ");
-        int n = sc.nextInt();
-       
+		System.out.println("Enter the value of n: ");
+		int n = sc.nextInt();
+		int a = 0;
+	    while(n>0) {
+	       int r = n % 10;
+	       a += r*r*r;
+	        n = n/10;	   
+	    }
+	    System.out.println("Sum of the digits is: " + a);
 	}
 	
 }
